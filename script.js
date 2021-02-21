e06be02e68f04cd5f9476d56d11e259bd0f6cffa
Hasip Timurtaş <hasip.timurtas@gmail.com>
2021-02-21 19:02:59 +0000

    applied changes

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principalEl = document.getElementById("principal");
+    var principal = principalEl.value;
+
+    if (principal < 1) {
+        alert("Enter a positive number");
+        principalEl.focus();
+        return
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var futureYear = new Date().getFullYear() + Number(years);
+
+
+    var htmlContent = `
+        If you deposit <mark>${principal}</mark>,<br>
+        at an interest rate of <mark>${rate}% </mark>.<br>
+        You will receive an amount of <mark>${interest}</mark>,<br>
+        in the year <mark>${futureYear}</mark>`;
+
+    document.getElementById("result").innerHTML = htmlContent;
+
 }
-        
+
+
+function writeRange() {
+    var rate = document.getElementById("rate").value;
+    rate = rate + " %";
+    document.getElementById("rate-value").innerHTML = rate
+}
